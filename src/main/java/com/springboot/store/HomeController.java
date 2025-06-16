dcaabb04ae30fc77a4162aeb034d238f443934f7
Aashi Pathak <64783500+aashi-pathak@users.noreply.github.com>
2025-06-16 15:17:33 +0900

    Configuring application properties

--- src/main/java/com/springboot/store/HomeController.java
+++ src/main/java/com/springboot/store/HomeController.java
@@ -1,12 +1,17 @@
 package com.springboot.store;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+    @Value("${spring.application.name}") // spring get value of this at runtime from application.properties
+    private String appName;
+
     @RequestMapping("/")
     public String index() {
+        System.out.println("appName: " + appName);
         return "index.html";
     }
 }
