96304aebb640208a00ea365176ab5efa37aca23e
Aashi Pathak <64783500+aashi-pathak@users.noreply.github.com>
2025-06-16 15:55:50 +0900

    Use setters for optional dependencies

--- src/main/java/com/springboot/store/OrderService.java
+++ src/main/java/com/springboot/store/OrderService.java
@@ -3,12 +3,16 @@ package com.springboot.store;
 public class OrderService {
     private PaymentService paymentService;
 
-    public OrderService(PaymentService paymentService) {
-        this.paymentService = paymentService;
-    }
+//    public OrderService(PaymentService paymentService) {
+//        this.paymentService = paymentService;
+//    }
 
     public void placeOrder() {
         paymentService.processPayment(10);
+    }
+
+    public void setPaymentService(PaymentService paymentService) {
+        this.paymentService = paymentService;
     }
 }
 
