2a4d24d6661df1850a2d7038815b79d8a4496e24
Aashi Pathak <64783500+aashi-pathak@users.noreply.github.com>
2025-06-16 16:11:39 +0900

    Annotations used in spring and beans

--- src/main/java/com/springboot/store/OrderService.java
+++ src/main/java/com/springboot/store/OrderService.java
@@ -1,18 +1,22 @@
 package com.springboot.store;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrderService {
     private PaymentService paymentService;
 
-//    public OrderService(PaymentService paymentService) {
-//        this.paymentService = paymentService;
-//    }
+    @Autowired // used when multiple constructors
+    public OrderService(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    public OrderService(PaymentService paymentService, int x) {}
+
 
     public void placeOrder() {
         paymentService.processPayment(10);
-    }
-
-    public void setPaymentService(PaymentService paymentService) {
-        this.paymentService = paymentService;
     }
 }
 
